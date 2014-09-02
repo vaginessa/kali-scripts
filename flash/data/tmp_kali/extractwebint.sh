207de51af66ac3a861e2fd14f5ba97e8bf5aeb85
muts <muts@pro.local>
2014-09-02 20:35:36 +0300

    Push relative fises everywhere

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -13,11 +13,11 @@ rm /data/local/kali-armhf/opt/mana/run-mana/conf/hostapd-karma.conf
 cd /data/local/kali-armhf/opt/mana/run-mana/conf/
 $busybox ln -s /sdcard/htdocs/files/hostapd-karma.conf hostapd-karma.conf
 
+rm /data/local/kali-armhf/etc/dnsmasq.conf
+cd /data/local/kali-armhf/etc/
+$busybox ln -s /sdcard/htdocs/files/dnsmasq.conf dnsmasq.conf
 
-
-cd /data/local/kali-armhf/opt/badandroid
 rm /data/local/kali-armhf/opt/badandroid/hosts
+cd /data/local/kali-armhf/opt/badandroid
 $busybox ln -s /sdcard/htdocs/files/hosts hosts
-$busybox ln -s /sdcard/htdocs/files/rev-tcp rev-tcp
-$busybox ln -s /sdcard/htdocs/files/rev-met rev-met
-$busybox ln -s /sdcard/htdocs/files/rev-met rev-met
+
