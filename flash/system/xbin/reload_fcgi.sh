9553fcc05481444cd2a9c24b193013b00f0854be
muts <muts@pro.local>
2014-09-02 17:48:11 +0300

    Fixed web paths and added start/stop scripts

--- flash/system/xbin/reload_fcgi.sh
+++ flash/system/xbin/reload_fcgi.sh
@@ -1,2 +1,2 @@
-kill -9 `ps | grep php | grep -v grep | awk '{print $2}'`
+kill -9 `ps | grep php | grep -v grep | awk '{print $2}'`  2>/dev/null
 /system/xbin/fcgiserver &
--- flash/system/xbin/reload_lighttpd.sh
+++ flash/system/xbin/reload_lighttpd.sh
@@ -1,2 +1,2 @@
-kill -9 `ps | grep lighttpd | grep -v grep | awk '{print $2}'`
+kill -9 `ps | grep lighttpd | grep -v grep | awk '{print $2}'`  2>/dev/null
 lighttpd -f /system/etc/lighttpd/lighttpd.conf
