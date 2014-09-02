07c27bbed356ebb159de7ef23d168829a0c65def
Binky Bear <binkybear@local>
2014-09-01 19:27:10 -1000

    symlink web server, startup script, touch log

---
+++ flash/data/local/userinit.sh
@@ -0,0 +1,61 @@
+busybox=/data/local/bin/busybox
+
+sleep 20
+
+#########  EXPORT ######### 
+#mount -o remount,rw -t yaffs2 /dev/block/mtdblock3 /system
+export bin=/system/bin
+export mnt=/data/local/kali-armhf
+PRESERVED_PATH=$PATH
+export PATH=/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/sbin:$PATH
+export TERM=xterm-256color
+export HOME=/root
+export LOGNAME=root
+
+#########  MOUNT ######### 
+
+$busybox mount -o bind /system $mnt/system
+$busybox mount -o bind /sdcard $mnt/sdcard
+$busybox mount -o bind /dev $mnt/dev
+$busybox mount -t devpts devpts $mnt/dev/pts
+$busybox mount -t proc proc $mnt/proc
+$busybox mount -t sysfs sysfs $mnt/sys
+		
+$busybox chmod 666 /dev/null
+
+# SET 250MB TO ALLOW POSTGRESQL #
+$busybox sysctl -w kernel.shmmax=268435456
+
+# NETWORK SETTINGS #
+$busybox sysctl -w net.ipv4.ip_forward=1
+echo "nameserver 208.67.222.222" > $mnt/etc/resolv.conf
+echo "nameserver 208.67.220.220" >> $mnt/etc/resolv.conf
+echo "127.0.0.1 localhost" > $mnt/etc/hosts
+echo "kali" > $mnt/proc/sys/kernel/hostname
+
+# execute startup script
+
+clear
+$busybox chroot $mnt /bin/bash -l
+
+chmod 755 /system/bin/bootkali
+chmod 755 /system/bin/killkali
+
+# start localhost webserver
+
+/system/xbin/reload_fcgi.sh
+/system/xbin/reload_lighttpd.sh
+
+# check startup options
+
+if [ -e "$mnt/root/.firstrun.option" ]; then
+        source $mnt/root/.firstrun.option
+        if [ $sshonboot == "1" ]; then
+			$busybox chroot $mnt service ssh start 
+        fi
+        if [ $vnconboot == "1" ]; then
+        	# remove old vnc servers so we can always start on display :1
+        	rm -rf $mnt/tmp/.X1* && rm -rf $mnt/root/.vnc/*.pid && rm -rf $mnt/root/.vnc/*.log
+            $busybox chroot $mnt su -c "vncserver -geometry ${RES} -depth 24 ${VNCLISTEN}" root
+        fi
+fi
