99d84b0ef1bb256098b49ba535cd4e4a74af8cb6
muts <muts@pro.local>
2014-09-02 20:11:50 +0300

    Fix log path to writable storage

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -9,22 +9,15 @@ $busybox chmod -R 0777 /sdcard/htdocs
 
 # Add mana configuration to webserver
 
+rm /data/local/kali-armhf/opt/mana/run-mana/conf/hostapd-karma.conf
 cd /data/local/kali-armhf/opt/mana/run-mana/conf/
-rm hostapd-karma.conf
 $busybox ln -s /sdcard/htdocs/files/hostapd-karma.conf hostapd-karma.conf
- 
-# Add hid scripts to webserver
 
-#cd /sdcard/htdocs/files
-#mv bad-usb hosts  
-#mv keyboard rev-tcp
-#mv test2.ps1 rev-met
 
-# Link badandroid host files to webserver 
- 
+
 cd /data/local/kali-armhf/opt/badandroid
-rm hosts
+rm /data/local/kali-armhf/opt/badandroid/hosts
 $busybox ln -s /sdcard/htdocs/files/hosts hosts
-
 $busybox ln -s /sdcard/htdocs/files/rev-tcp rev-tcp
+$busybox ln -s /sdcard/htdocs/files/rev-met rev-met
 $busybox ln -s /sdcard/htdocs/files/rev-met rev-met
