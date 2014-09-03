e803dc013e64bc1c9ce4559b08275246abafa19f
Binky Bear <binkybear@local>
2014-09-02 19:08:25 -1000

    remove unwanted /system/apps

---
+++ flash/data/tmp_kali/cleansystem.sh
@@ -0,0 +1,15 @@
+#!/sbin/sh
+#
+# clean the system
+#
+SA=/system/app
+rm -rf $SA/QuickOffice.apk
+rm -rf $SA/PrintSpooler.*  
+rm -rf $SA/QuickOffice.apk  
+rm -rf $SA/LatinImeGoogle* 
+rm -rf $SA/CloudPrint2.apk 
+rm -rf $SA/Hangouts.apk 
+rm -rf $SA/HPPrintPlugin.apk 
+rm -rf $SA/Books.apk 
+rm -rf $SA/KoreanIME.apk 
+rm -rf $SA/Magazines.apk
