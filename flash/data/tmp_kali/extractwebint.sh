d7d8c9b141c2ae57667c05f5ff76f37c0c1377da
muts <muts@pro.local>
2014-09-04 22:22:24 +0300

    Fix web links extraction

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -9,15 +9,18 @@ $busybox chmod -R 0777 /sdcard/htdocs
 
 # Add mana configuration to webserver
 
-rm /data/local/kali-armhf/opt/mana/run-mana/conf/hostapd-karma.conf
-cd /data/local/kali-armhf/opt/mana/run-mana/conf/
+rm /data/local/kali-armhf/root/mana/run-mana/conf/hostapd-karma.conf
+rm /data/local/kali-armhf/root/mana/run-mana/conf/dhcpd.conf
+rm /data/local/kali-armhf/root/mana/run-mana/conf/dnsspoof.conf
+
+
+cd /data/local/kali-armhf/root/mana/run-mana/conf/
 $busybox ln -s /sdcard/htdocs/files/hostapd-karma.conf hostapd-karma.conf
+$busybox ln -s /sdcard/htdocs/files/dhcpd.conf dhcpd.conf
+$busybox ln -s /sdcard/htdocs/files/dnsspoof.conf dnsspoof.conf
 
 rm /data/local/kali-armhf/etc/dnsmasq.conf
 cd /data/local/kali-armhf/etc/
 $busybox ln -s /sdcard/htdocs/files/dnsmasq.conf dnsmasq.conf
 
-rm /data/local/kali-armhf/opt/badandroid/hosts
-cd /data/local/kali-armhf/opt/badandroid
-$busybox ln -s /sdcard/htdocs/files/hosts hosts
 
