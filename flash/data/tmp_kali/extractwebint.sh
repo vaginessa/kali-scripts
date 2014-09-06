205cf06aad5f5f0b5a2e3904ca268ed41035c120
muts <muts@pro.local>
2014-09-07 00:57:05 +0300

    Working mana from web ootb

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -7,22 +7,3 @@ busybox=/tmp/busybox
 $busybox tar -zxf /tmp/htdocs.tar.gz -C /sdcard/
 $busybox chmod -R 0777 /sdcard/htdocs
 
-# Add mana configuration to webserver
-
-rm /data/local/kali-armhf/root/mana/run-mana/conf/hostapd-karma.conf
-rm /data/local/kali-armhf/root/mana/run-mana/conf/dhcpd.conf
-rm /data/local/kali-armhf/root/mana/run-mana/conf/dnsspoof.conf
-
-
-cd /data/local/kali-armhf/root/mana/run-mana/conf/
-ln -s /sdcard/htdocs/files/hostapd-karma.conf hostapd-karma.conf
-ln -s /sdcard/htdocs/files/dhcpd.conf dhcpd.conf
-ln -s /sdcard/htdocs/files/dnsspoof.conf dnsspoof.conf
-
-rm /data/local/kali-armhf/etc/dnsmasq.conf
-cd /data/local/kali-armhf/etc/
-ln -s /sdcard/htdocs/files/dnsmasq.conf dnsmasq.conf
-
-rm /data/local/kali-armhf/etc/dhcp/dhcpd.conf
-cd /data/local/kali-armhf/etc/dhcp/
-ln -s /sdcard/htdocs/files/dhcpd.conf dhcpd.conf
