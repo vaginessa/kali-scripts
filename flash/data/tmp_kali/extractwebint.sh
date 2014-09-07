947b53cf7d3c0de3fb05d24c87a776381704cc37
muts <muts@pro.local>
2014-09-07 18:29:11 +0300

    Update htdocs

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -7,3 +7,7 @@ busybox=/tmp/busybox
 $busybox tar -zxf /tmp/htdocs.tar.gz -C /sdcard/
 $busybox chmod -R 0777 /sdcard/htdocs
 
+rm /data/local/kali-armhf/etc/dnsmasq.conf
+cd /data/local/kali-armhf/etc/
+ln -s /sdcard/htdocs/files/dnsmasq.conf dnsmasq.conf
+
