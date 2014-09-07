5648dbfad5f1a86424ab9d434084dffd6114eb6e
muts <muts@pro.local>
2014-09-07 18:35:31 +0300

    Added webroot links

--- flash/data/tmp_kali/extractwebint.sh
+++ flash/data/tmp_kali/extractwebint.sh
@@ -7,7 +7,11 @@ busybox=/tmp/busybox
 $busybox tar -zxf /tmp/htdocs.tar.gz -C /sdcard/
 $busybox chmod -R 0777 /sdcard/htdocs
 
-rm /data/local/kali-armhf/etc/dnsmasq.conf
+rm -rf /data/local/kali-armhf/etc/dnsmasq.conf
 cd /data/local/kali-armhf/etc/
 ln -s /sdcard/htdocs/files/dnsmasq.conf dnsmasq.conf
 
+rm -rf /data/local/kali-armhf/etc/dhcp/dhcpd.conf
+mkdir -p /data/local/kali-armhf/etc/dhcp/
+cd /data/local/kali-armhf/etc/dhcp/
+ln -s /sdcard/htdocs/files/dhcpd.conf dhcpd.conf
