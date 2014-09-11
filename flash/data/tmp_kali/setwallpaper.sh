2c3eebc53a7750562f34e964f105f1673dbb33e6
Binky Bear <binkybear@local>
2014-09-11 06:41:48 -1000

    fix wallpaper / remove startup options kalimenu

--- flash/data/tmp_kali/setwallpaper.sh
+++ flash/data/tmp_kali/setwallpaper.sh
@@ -2,6 +2,9 @@
 #
 # Set the wallpaper based on device
 #
+chmod 777 /data/system/users/0/wallpaper
+rm /data/system/users/0/wallpaper
+
 if [ $(getprop ro.product.device) == "manta" ]; then
 	cp /tmp/mantawallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
@@ -23,11 +26,11 @@ if [ $(getprop ro.product.device) == "hammerhead" ]; then
 fi
 
 if [ $(getprop ro.product.device) == "grouper" ]; then
-	cp /tmp/mantawallpaper /data/system/users/0/wallpaper
+	cp /tmp/grouperwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "tilapia" ]; then
-	cp /tmp/mantawallpaper /data/system/users/0/wallpaper
+	cp /tmp/grouperwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 fi
